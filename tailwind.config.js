b902cc52eb7a5563d4c4f20ebd4743603e10c685
newman15 <aaronnewman1515@gmail.com>
2022-05-29 15:49:02 -0700

    Installed tailwindCSS and uploaded landing page images

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+
+    theme: {
+        fontFamily: {
+            
+        },
+
+        screens:{
+            'xs': '360px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+            'carouselWidth': '700px'
+        }
+    },
+
+    plugins: [],
+}
