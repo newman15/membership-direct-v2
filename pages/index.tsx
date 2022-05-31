2cdac44f42b6363071ef2ab453101cbc137167c6
newman15 <aaronnewman1515@gmail.com>
2022-05-31 16:00:35 -0700

    Fixed Hero Image, and added text, and membership flex images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import heroImage from '../public/driving.jpg';
+import titleText from '../public/title.svg';
+import basicMember from '../public/basic_membership.png';
+import premiumMember from '../public/premium_membership.png';
 
 const Home: NextPage = () => {
   return (
@@ -14,20 +18,76 @@ const Home: NextPage = () => {
       <main>
 
         {/* Hero Image with title text */}
-        <div className='relative'>
-            <Image
-                src="/driving.jpg"
-                layout='responsive'
-                width={100}
-                height={40}
-            />
+        <header>
+            <div className='relative top-0 left-0'>
+                <Image
+                    src={heroImage}
+                    layout='responsive'
+                    width={100}
+                    height={40}
+                    priority
+                />
 
-            <div className='absolute top-[12%] left-1/2 font-CinzelDecorative font-bold 
-            text-white text-titleText -translate-x-1/2 -translate-y-1/2 backdrop-blur-[3px]
-            drop-shadow-2xl'>
-                Membership Direct
+
+                <div className='absolute w-[50%] top-[1%] left-[25%]'>
+                    <Image
+                        src={titleText}
+                        layout='responsive'
+                        width={1}
+                        height={.20}
+                        priority
+                    />
+                </div>
+
+
+                {/* <div className='absolute top-[12%] left-1/2 font-CinzelDecorative font-bold 
+                text-white text-titleText -translate-x-1/2 -translate-y-1/2 backdrop-blur-[3px]
+                drop-shadow-2xl'>
+                    Membership Direct
+                </div> */}
             </div>
-        </div>
+        </header>
+
+        <section>
+            <div className='text-center m-6'>
+                <h5 className='mb-6'>Get To Know Us</h5>
+                <h4 className='mb-6 text'>Our Statement</h4>
+                <p className='max-w-[800px] mx-auto text-left'>It is our mission at Membership Direct to provide an additional solution to alleviate 
+                    the financial burdens that accidents can cause. For a low annual membership fee, 
+                    users will be able to choose their membership type that can be redeemed at any approved 
+                    auto repair shop of their choosing, after successfully filing a claim with their insurance 
+                    provider.
+                </p>
+            </div>
+        </section>
+
+        <section>
+            <div className='text-center mt-24'>
+                <h4>2 Membership Types</h4>
+                <div className='flex flex-row justify-center'>
+                    <div className='w-[400px]'>
+                        <Image
+                            src={basicMember}
+                            layout='responsive'
+                            width={1}
+                            height={1}
+                            priority
+                        />
+                    </div>
+                    
+                    <div className='w-[400px]'>
+                        <Image
+                            src={premiumMember}
+                            layout='responsive'
+                            width={1}
+                            height={1}
+                            priority
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+        
 
       </main>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
@@ -29,5 +31,16 @@ module.exports = {
         }
     },
 
-    plugins: [],
+    plugins: [
+        plugin(function({ addBase, theme }) {
+            addBase({
+              'h1': { fontSize: theme('fontSize.6xl') },
+              'h2': { fontSize: theme('fontSize.5xl') },
+              'h3': { fontSize: theme('fontSize.4xl') },
+              'h4': { fontSize: theme('fontSize.3xl') },
+              'h5': { fontSize: theme('fontSize.2xl') },
+              'h6': { fontSize: theme('fontSize.xl') },
+            })
+          })
+    ],
   }
